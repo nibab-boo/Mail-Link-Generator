0c5e9d307cee2eca3047d91688437f7ae27671d5
Babin Bohora <bohorababin1@gmail.com>
2022-07-21 14:13:15 +0900

    App with components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import FormElement from './components/FormElement'
+import OutputLink from './components/OutputLink'
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState("");
+
+  const getLink = (link) => {
+    console.log("link", link);
+    setData(link);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-body">
+        <h3>Generate Link</h3>
+        <FormElement sendLink={getLink} />
+        {
+          data && (
+            <OutputLink data={data}/>
+          )
+        }
+      </div>
     </div>
   );
 }
