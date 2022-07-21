f4e3648443ce1d2516f102cdabdea38cd0077822
Babin Bohora <bohorababin1@gmail.com>
2022-07-21 14:13:48 +0900

    Outlink done

---
+++ src/components/OutputLink.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+
+const OutputLink = ({data}) => {
+
+  const [link, setLink] = useState(data);
+
+  const copyLink = () => {
+    navigator.clipboard.writeText(link);
+  }
+
+  return (
+    <div className='mb-3 mt-5 w-75'>
+      <textarea onChange={(e) => setLink(e.currentTarget.value)} class="form-control"  row="3" name="link" id="link" value={link}/>
+      <button onClick={copyLink} className='btn btn-primary btn-sm'>Copy</button>
+    </div>
+  )
+};
+
+export default OutputLink;
